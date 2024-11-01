8084936ea8a292a6108b5fa80ff4c1c2fc84b57b
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-01 18:54:14 +0200

    add a model, create second view

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,17 +6,49 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @Query var destinations: [Destination]
+    @State private var path = [Destination]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List {
+                ForEach(destinations) { destination in
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+                            
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
+                    }
+                }
+                .onDelete(perform: deleteDestinations)
+            }
+            .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+            .toolbar {
+                Button("Add Destination", systemImage: "plus", action: addDestination)
+            }
         }
-        .padding()
     }
+    
+    func addDestination() {
+        let destination = Destination()
+        context.insert(destination)
+        path = [destination]
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            context.delete(destination)
+        }
+    }
+    
 }
 
 #Preview {
