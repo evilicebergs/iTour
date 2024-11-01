35400888d429807e204b60bbefab903839581fc9
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-01 17:02:34 +0200

    Initial Commit

---
+++ iTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour
+//
+//  Created by Artem Golovchenko on 2024-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Artem Golovchenko on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
